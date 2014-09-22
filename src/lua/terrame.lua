27a0939e13daf0c9212ac2ffb4dd068eb63d8551
Pedro R. Andrade <pedro.andrade@inpe.br>
2014-09-22 09:41:38 -0300

    Header added.

--- src/lua/terrame.lua
+++ src/lua/terrame.lua
@@ -1,3 +1,29 @@
+-------------------------------------------------------------------------------------------
+-- TerraME - a software platform for multiple scale spatially-explicit dynamic modeling.
+-- Copyright (C) 2001-2014 INPE and TerraLAB/UFOP -- www.terrame.org
+-- 
+-- This code is part of the TerraME framework.
+-- This framework is free software; you can redistribute it and/or
+-- modify it under the terms of the GNU Lesser General Public
+-- License as published by the Free Software Foundation; either
+-- version 2.1 of the License, or (at your option) any later version.
+-- 
+-- You should have received a copy of the GNU Lesser General Public
+-- License along with this library.
+-- 
+-- The authors reassure the license terms regarding the warranties.
+-- They specifically disclaim any warranties, including, but not limited to,
+-- the implied warranties of merchantability and fitness for a particular purpose.
+-- The framework provided hereunder is on an "as is" basis, and the authors have no
+-- obligation to provide maintenance, support, updates, enhancements, or modifications.
+-- In no event shall INPE and TerraLAB / UFOP be held liable to any party for direct,
+-- indirect, special, incidental, or consequential damages arising out of the use
+-- of this library and its documentation.
+-- 
+-- Authors:
+--       Pedro R. Andrade (pedro.andrade@inpe.br)
+--       Raian V. Maretto
+-------------------------------------------------------------------------------------------
 
 -- Creates a text file with the names of the files inside a given folder
 local function dir(folder)
