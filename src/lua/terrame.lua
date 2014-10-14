1a5e1fabcb4c57ec124d536a4a188fe530286618
Pedro Andrade <pedro.andrade@inpe.br>
2014-10-14 09:48:55 -0300

    traceback() added.

--- src/lua/terrame.lua
+++ src/lua/terrame.lua
@@ -659,16 +659,16 @@ executeTests = function(fileName, package)
 					print_red(...)
 				end
 
-				local ok_execution, err = pcall(function() include(baseDir..s.."examples"..s..value) end)
+				xpcall(function() include(baseDir..s.."examples"..s..value) end, function(err)
+					ut.examples_error = ut.examples_error + 1
+					print_red(err)
+					print_red(traceback())
+				end)
 
 				print = print__
 
 				killAllObservers()
 		
-				if not ok_execution then
-					ut.examples_error = ut.examples_error + 1
-					print_red(err)
-				end
 			end)
 		end
 	else
@@ -873,6 +873,60 @@ function getLevel()
 	end
 end
 
+function traceback()
+	local level = 1
+
+	local str = ""
+	str = str.."Stack traceback:\n"
+
+	local last_function = ""
+	local found_function = false
+
+	local info = debug.getinfo(level)
+	while info ~= nil do
+		local m1 = string.match(info.source, replaceSpecialChars(sessionInfo().path.."/lua"))
+		local m2 = string.match(info.source, replaceSpecialChars(sessionInfo().path.."/packages/base/lua"))
+		local m3 = string.match(info.short_src, "%[C%]")
+		if m1 or m2 or m3 then
+			last_function = info.name
+		else
+			if not found_function then
+				if     last_function == "__add"      then last_function = "operator + (addition)"
+				elseif last_function == "__sub"      then last_function = "operator - (subtraction)"
+				elseif last_function == "__mul"      then last_function = "operator * (multiplication)"
+				elseif last_function == "__div"      then last_function = "operator / (division)"
+				elseif last_function == "__mod"      then last_function = "operator % (modulo)"
+				elseif last_function == "__pow"      then last_function = "operator ^ (exponentiation)"
+				elseif last_function == "__unm"      then last_function = "operator - (minus)"
+				elseif last_function == "__concat"   then last_function = "operator .. (concatenation)"
+				elseif last_function == "__len"      then last_function = "operator # (size)"
+				elseif last_function == "__eq"       then last_function = "operator == (equal)"
+				elseif last_function == "__lt"       then last_function = "comparison operator"
+				elseif last_function == "__le"       then last_function = "comparison operator"
+				elseif last_function == "__index"    then last_function = "operator [] (index)"
+				elseif last_function == "__newindex" then last_function = "operator [] (index)"
+				elseif last_function == "__call"     then last_function = "call"
+				else   last_function = "function "..last_function
+				end
+
+				str = str.. "    In "..last_function.."\n"
+				found_function = true
+			end
+
+			str = str.."    File "..info.short_src..", line "..info.currentline
+			if info.name then
+				str = str..", in function "..info.name
+			else
+				str = str..", in main chunk"
+			end
+			str = str.."\n"
+		end
+		level = level + 1
+		info = debug.getinfo(level)
+	end
+	return string.sub(str, 0, string.len(str) - 1)
+end
+
 -- RAIAN implementar
 execute = function(parameters) -- parameters is a string
 	-- implementa o sessionInfo
@@ -966,60 +1020,6 @@ execute = function(parameters) -- parameters is a string
 			else
 				require("base")
 			end
-			local function getLevel()
-				local level = 1
-
-				local str = ""
-				str = str.."Stack traceback:\n"
-
-				local last_function = ""
-				local found_function = false
-
-				local info = debug.getinfo(level)
-				while info ~= nil do
-					local m1 = string.match(info.source, replaceSpecialChars(sessionInfo().path.."/lua"))
-					local m2 = string.match(info.source, replaceSpecialChars(sessionInfo().path.."/packages/base/lua"))
-					local m3 = string.match(info.short_src, "%[C%]")
-					if m1 or m2 or m3 then
-						last_function = info.name
-					else
-						if not found_function then
-							if     last_function == "__add"      then last_function = "operator + (addition)"
-							elseif last_function == "__sub"      then last_function = "operator - (subtraction)"
-							elseif last_function == "__mul"      then last_function = "operator * (multiplication)"
-							elseif last_function == "__div"      then last_function = "operator / (division)"
-							elseif last_function == "__mod"      then last_function = "operator % (modulo)"
-							elseif last_function == "__pow"      then last_function = "operator ^ (exponentiation)"
-							elseif last_function == "__unm"      then last_function = "operator - (minus)"
-							elseif last_function == "__concat"   then last_function = "operator .. (concatenation)"
-							elseif last_function == "__len"      then last_function = "operator # (size)"
-							elseif last_function == "__eq"       then last_function = "operator == (equal)"
-							elseif last_function == "__lt"       then last_function = "comparison operator"
-							elseif last_function == "__le"       then last_function = "comparison operator"
-							elseif last_function == "__index"    then last_function = "operator [] (index)"
-							elseif last_function == "__newindex" then last_function = "operator [] (index)"
-							elseif last_function == "__call"     then last_function = "call"
-							else   last_function = "function "..last_function
-							end
-
-							str = str.. "    In "..last_function.."\n"
-							found_function = true
-						end
-
-						str = str.."    File "..info.short_src..", line "..info.currentline
-						if info.name then
-							str = str..", in function "..info.name
-						else
-							str = str..", in main chunk"
-						end
-						str = str.."\n"
-					end
-					level = level + 1
-					info = debug.getinfo(level)
-				end
-				return string.sub(str, 0, string.len(str) - 1)
-			end
-
 			local success, result = xpcall(function() dofile(param) end, function(err)
 				local luaFolder = replaceSpecialChars(sessionInfo().path.."/lua")
 				local baseLuaFolder = replaceSpecialChars(sessionInfo().path.."/packages/base/lua")
@@ -1065,7 +1065,7 @@ execute = function(parameters) -- parameters is a string
 					return string.sub(str, 0, string.len(str) - 1)
 
 				else
-					return err.."\n"..getLevel()
+					return err.."\n"..traceback()
 				end
 			end)
 
