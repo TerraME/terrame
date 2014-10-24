15fc551407cf815b883b99a5fd65ba3a4c798bcb
Pedro Andrade <pedro.andrade@inpe.br>
2014-10-24 12:08:11 -0200

    New examples added.

---
+++ base/examples/growing-society.lua
@@ -0,0 +1,83 @@
+EMPTY = 0
+FULL = 1
+
+singleFooAgent = Agent {
+	execute = function(self)
+		cell = self:getCell():getNeighborhood():sample()
+		if cell.state == EMPTY and math.random() < 0.3 then
+			child = self:reproduce()
+			child:move(cell)
+			cell.state = FULL
+		end
+		cell = self:getCell():getNeighborhood():sample()
+		if cell.state == EMPTY then
+			self:move(cell)
+		end
+	end
+}
+
+soc = Society{
+	instance = singleFooAgent,
+	quantity = 10
+}
+
+cs = CellularSpace {
+	xdim = 100
+}
+
+cs:createNeighborhood()
+
+e = Environment {
+	cs,
+	soc
+}
+
+e:createPlacement{strategy = "random", max = 1}
+
+c = Cell{quantity = 1}
+
+o = Observer{
+	subject = c,
+	type = "chart",
+	xLabel = "Time",
+	yLabel = "Number of Agents",
+	attributes = {"quantity"}
+}
+
+c:notify(0)
+
+t = Timer {
+	Event{action = soc},
+	Event{action = cs},
+	Event{action = function(e)
+		c.quantity = #soc
+		c:notify(e:getTime())
+	end}
+}
+
+forEachCell(cs, function(cell)
+	cell.state = EMPTY
+end)
+
+forEachAgent(soc, function(agent)
+	agent:getCell().state = FULL
+end)
+
+--[[
+leg = Legend {
+	grouping = "uniquevalue",
+	colorBar = {
+		{value = EMPTY, color = "white"},
+		{value = FULL, color = "black"}
+	}
+}
+
+Observer {
+	subject = cs,
+	attributes = {"state"},
+	legends = {leg}
+}
+--]]
+
+t:execute(120)
+
---
+++ base/examples/water-in-the-tube.lua
@@ -0,0 +1,21 @@
+
+world = Cell{water = 40}
+
+o = Observer{
+    subject = world,
+    type = "chart",
+	xLabel = "Time",
+	yLabel = "Gallons",
+    attributes = {"water"}
+}
+
+world:notify(0)
+
+t = Timer {
+    Event {action = function(e)
+        world.water = world.water - 5
+        world:notify(e:getTime())
+    end}
+}
+
+t:execute(8)
