ccc7dcd70a5b07e805f9e97b3577cfd2911941d7
Pedro Andrade <pedro.andrade@inpe.br>
2015-03-24 15:37:15 -0300

    Tests to execute all lines of Clock.

--- base/tests/observer/basics/Clock.lua
+++ base/tests/observer/basics/Clock.lua
@@ -1,43 +1,44 @@
---#########################################################################################
+-------------------------------------------------------------------------------------------
 -- TerraME - a software platform for multiple scale spatially-explicit dynamic modeling.
--- Copyright (C) 2001-2014 INPE and TerraLAB/UFOP -- www.terrame.org
--- 
+-- Copyright (C) 2001-2014 INPE and TerraLAB/UFOP.
+--
 -- This code is part of the TerraME framework.
 -- This framework is free software; you can redistribute it and/or
 -- modify it under the terms of the GNU Lesser General Public
 -- License as published by the Free Software Foundation; either
 -- version 2.1 of the License, or (at your option) any later version.
--- 
+--
 -- You should have received a copy of the GNU Lesser General Public
 -- License along with this library.
--- 
+--
 -- The authors reassure the license terms regarding the warranties.
 -- They specifically disclaim any warranties, including, but not limited to,
 -- the implied warranties of merchantability and fitness for a particular purpose.
 -- The framework provided hereunder is on an "as is" basis, and the authors have no
 -- obligation to provide maintenance, support, updates, enhancements, or modifications.
 -- In no event shall INPE and TerraLAB / UFOP be held liable to any party for direct,
--- indirect, special, incidental, or consequential damages arising out of the use
+-- indirect, special, incidental, or caonsequential damages arising out of the use
 -- of this library and its documentation.
--- 
--- Authors: Pedro R. Andrade (pedro.andrade@inpe.br)
---#########################################################################################
+--
+-- Authors: Pedro R. Andrade
+-------------------------------------------------------------------------------------------
 
---- Create a display with the current time and Event queue of a given Timer.
--- @arg data.subject A timer.
--- @usage Clock{subject = timer}
-Clock = function(data)
-	mandatoryTableArgument(data, "subject", "Timer")
+return{
+	Clock = function(unitTest)
+		local timer
 
-	checkUnnecessaryArguments(data, {"subject"})
+		timer = Timer{
+			ev1 = Event{priority =  1, action = function(event) timer:notify() end},
+			ev2 = Event{priority = 10, action = function(event) timer:notify() end},
+			ev3 = Event{priority = 10, action = function(event) timer:notify() end},
+			ev4 = Event{priority = 10, action = function(event) timer:notify() end}
+		}
 
-	local observerAttrs = {}
-	local observerParams = {"", ""}
-	local observerType = 8
+		Clock{subject = timer}
+		timer:execute(50)
 
-	local id = data.subject.cObj_:createObserver(observerType, observerAttrs, observerParams)
-
-    table.insert(createdObservers, {subject = data.subject, id = id})
-	return id
-end
+		unitTest:assert(true)
+		unitTest:delay()
+	end
+}
 
