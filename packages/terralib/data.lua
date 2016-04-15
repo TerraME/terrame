7e22b0ac0f2b08a4826153f650a5f6d2526e1590
pedro-andrade-inpe <pedro.andrade@inpe.br>
2016-04-15 10:00:55 -0300

    Moving fillcell files to terralib. #826

--- packages/terralib/data.lua
+++ packages/terralib/data.lua
@@ -21,3 +21,40 @@ data{
 	summary = "Blank Project used for internal control system",
 	source = "TerraME Team"
 }
+
+data{
+	file = {"Setores_Censitarios_2000_pol.dbf", "Setores_Censitarios_2000_pol.shp", "Setores_Censitarios_2000_pol.shx", "Setores_Censitarios_2000_pol.prj", "Setores_Censitarios_2000_props.txt"},
+	summary = "",
+	source = ""
+}
+
+data{
+	file = {"Rodovias_lin.dbf", "Rodovias_lin.shp", "Rodovias_lin.shx", "Rodovias_lin.prj", "Rodovias_props.txt"},
+	summary = "",
+	source = ""
+}
+
+data{
+	file = {"Localidades_pt.dbf", "Localidades_pt.shp", "Localidades_pt.shx", "Localidades_pt.prj", "Localidades_props.txt"},
+	summary = "",
+	source = ""
+}
+
+data{
+	file = {"Desmatamento_2000.tif", "Desmatamento_2000.xml"},
+	summary = "",
+	source = ""
+}
+
+data{
+	file = {"altimetria.tif", "Altimetria.xml"},
+	summary = "",
+	source = ""
+}
+
+data{
+	file = {"amazonia.tview"},
+	summary = "",
+	source = ""
+}
+
