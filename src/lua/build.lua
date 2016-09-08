f5d994756e7b5065bed7c610663878287031a873
Pedro Andrade <pedro.andrade@inpe.br>
2016-09-08 09:22:00 -0300

    Close #1407. Verifying if md5 software exists.

--- src/lua/build.lua
+++ src/lua/build.lua
@@ -297,8 +297,12 @@ function _Gtme.buildPackage(package, config, clean)
 	
 	if _Gtme.isWindowsOS() then 
 		md5sum = runCommand("md5sum "..file) 
-	else 
+	elseif runCommand("which md5")[1] then
 		md5sum = runCommand("md5 -q "..file) 
+	elseif runCommand("which md5sum")[1] then
+		md5sum = runCommand("md5sum -q "..file)
+	else
+		printWarning("Could not find an MD5 sum software installed.")
 	end
 
 	chDir(currentdir)
