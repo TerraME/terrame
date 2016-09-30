e993af1cb0654bff35dbd076acb43f8264c215fa
Rodrigo Avancini <avancinirodrigo@yahoo.com.br>
2016-09-30 16:43:34 -0300

    Small correction #1379.

--- packages/terralib/tests/postgis/alternative/Layer.lua
+++ packages/terralib/tests/postgis/alternative/Layer.lua
@@ -358,9 +358,11 @@ return {
 		unitTest:assertError(userNotExists, "It was not possible to create a connection to the given data source due to the following error: "
 							.."FATAL:  password authentication failed for user \""..nonuser.."\"\n.", 64) -- #1303
 
-		local wrongPass = "passiswrong"
+		local wrongPass
+		local passWrong
 	if sessionInfo().system ~= "mac" then -- TODO(#1379)
-		local passWrong = function()
+		wrongPass = "passiswrong"
+		passWrong = function()
 			Layer{
 				project = proj1,
 				source = "postgis",
