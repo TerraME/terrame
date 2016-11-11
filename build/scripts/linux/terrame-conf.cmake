eba5196ee6cda1caa87960e49440f80bb3a8e3e4
raphaelrpl <raphael.wcosta@gmail.com>
2016-11-11 17:31:58 -0200

    Close #1528 - moving cmake vars _TERRAME_BUILD_AS_BUNDLE and _TERRAME_CREATE_INSTALLER to .sh|.batch script

--- build/scripts/linux/terrame-conf.cmake
+++ build/scripts/linux/terrame-conf.cmake
@@ -30,18 +30,8 @@ set(CMAKE_BUILD_TYPE Release CACHE STRING "Build type" FORCE)
 set(CMAKE_INSTALL_PREFIX "$ENV{_TERRAME_INSTALL_PATH}" CACHE PATH "Where to install TerraME?" FORCE)
 set(CMAKE_PREFIX_PATH "${TERRALIB_3RDPARTY_DIR};${Qt5_DIR};${TERRAME_DEPENDENCIES_DIR}" CACHE PATH "Where are the dependencies of TerraME?" FORCE)
 set(TERRAME_VERSION_STATUS "beta-4.1" CACHE STRING "Define name of installer" FORCE)
-
-if (NOT DEFINED ENV{_TERRAME_BUILD_AS_BUNDLE})
-	set(TERRAME_BUILD_AS_BUNDLE OFF CACHE BOOL "If on, tells that the build will generate a bundle" FORCE)
-else()
-	set(TERRAME_BUILD_AS_BUNDLE $ENV{_TERRAME_BUILD_AS_BUNDLE} CACHE BOOL "If on, tells that the build will generate a bundle" FORCE)
-endif()
-
-if (NOT DEFINED ENV{_TERRAME_CREATE_INSTALLER})
-	set(TERRAME_CREATE_INSTALLER OFF CACHE BOOL "Create the installer" FORCE)
-else()
-	set(TERRAME_CREATE_INSTALLER $ENV{_TERRAME_CREATE_INSTALLER} CACHE BOOL "Create the installer" FORCE)
-endif()
+set(TERRAME_BUILD_AS_BUNDLE $ENV{_TERRAME_BUILD_AS_BUNDLE} CACHE BOOL "If on, tells that the build will generate a bundle" FORCE)
+set(TERRAME_CREATE_INSTALLER $ENV{_TERRAME_CREATE_INSTALLER} CACHE BOOL "Create the installer" FORCE)
 
 # Dependencies paths
 set(QTLUA_INCLUDE_DIR "$ENV{_TERRALIB_3RDPARTY_DIR}/include/QtLua" CACHE PATH "QtLua include" FORCE)
