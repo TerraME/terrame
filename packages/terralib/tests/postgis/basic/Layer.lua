75b1083eb68ac52ac607d31a09577e63da3d0a07
Pedro Andrade <pedro.andrade@inpe.br>
2017-01-05 16:46:32 -0200

    Updating year.

--- packages/terralib/tests/postgis/basic/Layer.lua
+++ packages/terralib/tests/postgis/basic/Layer.lua
@@ -1,6 +1,6 @@
 -------------------------------------------------------------------------------------------
 -- TerraME - a software platform for multiple scale spatially-explicit dynamic modeling.
--- Copyright (C) 2001-2016 INPE and TerraLAB/UFOP -- www.terrame.org
+-- Copyright (C) 2001-2017 INPE and TerraLAB/UFOP -- www.terrame.org
 
 -- This code is part of the TerraME framework.
 -- This framework is free software; you can redistribute it and/or
@@ -219,7 +219,7 @@ return {
 
 		unitTest:assertEquals(l3.name, clName3)
 
-		local newDbName = "new_pg_db_30032016"
+		local newDbName = "new_pg_db_30032017"
 		pgData.database = newDbName
 		tl:dropPgDatabase(pgData)
 		pgData.database = database
