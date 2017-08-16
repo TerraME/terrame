1d8c751e0464a25450e97936697f1ff9534d437e
Rodrigo Avancini <avancinirodrigo@yahoo.com.br>
2017-08-16 16:50:27 -0300

    Up #1923.

--- jenkins/linux/terrame-environment-linux-ubuntu-14.04.sh
+++ jenkins/linux/terrame-environment-linux-ubuntu-14.04.sh
@@ -22,17 +22,37 @@
 # indirect, special, incidental, or consequential damages arising out of the use
 # of this software and its documentation.
 
+#
+# Valid parameter val or abort script
+#
+function valid()
+{
+  if [ $1 -ne 0 ]; then
+    echo $2
+    echo ""
+    exit 1
+  fi
+}
+
 ####################### GitHub Triggers
 if [ ! -z "$ghprbActualCommit" ]; then
 	echo "Triggering All Builds"
 	/home/jenkins/Configs/terrame/status/send.sh $ghprbActualCommit "C++ Syntax" "pending" "$BUILD_URL/consoleFull" "Build Triggered"
+	sleep 1s
 	/home/jenkins/Configs/terrame/status/send.sh $ghprbActualCommit "Linux Compilation" "pending" "$BUILD_URL/consoleFull" "Build Triggered"
+	sleep 1s
 	/home/jenkins/Configs/terrame/status/send.sh $ghprbActualCommit "Code analysis of package base" "pending" "$BUILD_URL/consoleFull" "Build Triggered"
+	sleep 1s
 	/home/jenkins/Configs/terrame/status/send.sh $ghprbActualCommit "Code analysis of package gis" "pending" "$BUILD_URL/consoleFull" "Build Triggered"
+	sleep 1s
 	/home/jenkins/Configs/terrame/status/send.sh $ghprbActualCommit "Documentation of package base" "pending" "$BUILD_URL/consoleFull" "Build Triggered"
+	sleep 1s
 	/home/jenkins/Configs/terrame/status/send.sh $ghprbActualCommit "Documentation of package gis" "pending" "$BUILD_URL/consoleFull" "Build Triggered"
+	sleep 1s
 	/home/jenkins/Configs/terrame/status/send.sh $ghprbActualCommit "Functional test of package base" "pending" "$BUILD_URL/consoleFull" "Build Triggered"
+	sleep 1s
 	/home/jenkins/Configs/terrame/status/send.sh $ghprbActualCommit "Functional test of package gis" "pending" "$BUILD_URL/consoleFull" "Build Triggered"
+	sleep 1s
 	/home/jenkins/Configs/terrame/status/send.sh $ghprbActualCommit "Execution Test" "pending" "$BUILD_URL/consoleFull" "Build Triggered"
 fi
 
@@ -41,7 +61,7 @@ echo "### TerraLib Environment ###"
 
 echo "Cleaning last config scripts"
 rm -rf $_TERRALIB_BUILD_BASE/solution/terralib-conf.*
-valid $? "Error: Clean fail"
+valid $? "Error: Cleaning fail"
 
 cd $_TERRALIB_GIT_DIR
 
@@ -52,11 +72,50 @@ if [ $(git status --porcelain) ]; then # Check if TerraLib must be compiled
 	if [ ! -z "$ghprbActualCommit" ]; then
 		echo "Cleaning last install"
 		rm -rf $_TERRALIB_OUT_DIR/terralib_mod_binding_lua  $_TERRALIB_INSTALL_PATH # Removing TerraLib Mod Binding Lua in order to re-generate it
-		valid $? "Error: Clean fail"
+		valid $? "Error: Cleaning fail"
 	else
 		echo "TODO"
 	fi
+	
+	echo "Copying TerraLib compilation scripts to TerraLib Solution folder"
+	cp --verbose $_TERRAME_GIT_DIR/build/scripts/linux/terralib-conf.* $_TERRALIB_BUILD_BASE/solution
+	valid $? "Error: Copying fail"	
 fi
 
 echo "### TerraLib Environment Finished ###"
+
+##################### TerraME Environment
+echo "### TerraME Environment ###"
+
+echo "Cleaning last directories"
+rm -rf $_TERRAME_BUILD_BASE/solution $_TERRAME_REPOSITORY_DIR $_TERRAME_TEST_DIR $_TERRAME_EXECUTION_DIR
+valid $? "Error: Cleaning fail"
+
+echo "Creating TerraME Test folders and TerraLib solution"
+mkdir $_TERRAME_BUILD_BASE/solution $_TERRAME_REPOSITORY_DIR $_TERRAME_TEST_DIR $_TERRAME_EXECUTION_DIR 
+valid $? "Error: Creating fail"
+
+echo "Copying TerraME Git Repository to Test Repository Folder"
+cp --verbose -r $_TERRAME_GIT_DIR/repository/* $_TERRAME_REPOSITORY_DIR
+valid $? "Error: Copying fail"	
+cp --verbose $_TERRAME_GIT_DIR/jenkins/linux/terrame-repository-test-linux-ubuntu-14.04.sh $_TERRAME_REPOSITORY_DIR
+valid $? "Error: Copying fail"	
+
+echo "Copying TerraME Git Test Execution to Test Execution Folder"
+cp --verbose -r $_TERRAME_GIT_DIR/test/* $_TERRAME_EXECUTION_DIR
+valid $? "Error: Copying fail"	
+cp --verbose $_TERRAME_GIT_DIR/jenkins/linux/terrame-test-execution-linux-ubuntu-14.04.sh $_TERRAME_EXECUTION_DIR
+valid $? "Error: Copying fail"	
+
+echo "Copying TerraME test and config file to Test folder"
+cp --verbose $_TERRAME_GIT_DIR/jenkins/all/*.lua $_TERRAME_TEST_DIR
+valid $? "Error: Copying fail"
+cp --verbose $_TERRAME_GIT_DIR/jenkins/linux/terrame-unittest-linux-ubuntu-14.04.sh $_TERRAME_TEST_DIR
+valid $? "Error: Copying fail"
+cp --verbose $_TERRAME_GIT_DIR/jenkins/linux/terrame-code-analysis-linux-ubuntu-14.04.sh $_TERRAME_TEST_DIR
+valid $? "Error: Copying fail"
+cp --verbose $_TERRAME_GIT_DIR/jenkins/linux/terrame-doc-linux-ubuntu-14.04.sh $_TERRAME_TEST_DIR
+valid $? "Error: Copying fail"
+cp --verbose $_TERRAME_GIT_DIR/jenkins/linux/terrame-syntaxcheck-cpp-linux-ubuntu-14.04.sh $_TERRAME_TEST_DIR
+valid $? "Error: Copying fail"
 
