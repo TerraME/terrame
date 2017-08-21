28c3ba2995b4df00edceb862ebfeb4bca197d573
Rodrigo Avancini <avancinirodrigo@yahoo.com.br>
2017-08-21 14:40:52 -0300

    Up #1923.

--- jenkins/linux/terrame-doc-linux-ubuntu-14.04.sh
+++ jenkins/linux/terrame-doc-linux-ubuntu-14.04.sh
@@ -34,8 +34,6 @@ exit 0
 ##
 #
 
-$_TERRAME_BUILD_BASE/solution/terrame-git-notify-linux-ubuntu-14.04.sh $ghprbActualCommit $_TERRAME_GITHUB_STATUS_CONTEXT -1 "$BUILD_URL/consoleFull"
-
 # Exporting context
 export TME_PATH=$_TERRAME_INSTALL_PATH/bin
 export PATH=$PATH:$TME_PATH
@@ -51,4 +49,4 @@ fi
 # Execute TerraME doc generation
 terrame -color $TERRAME_COMMANDS -doc 2> /dev/null
 
-$_TERRAME_BUILD_BASE/solution/terrame-git-notify-linux-ubuntu-14.04.sh $ghprbActualCommit $_TERRAME_GITHUB_STATUS_CONTEXT $? "$BUILD_URL/consoleFull"
+exit $?
