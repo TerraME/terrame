c9cc29d6e76243a70709ee4177696cbbbf41d343
Pedro Andrade <pedro.andrade@inpe.br>
2017-08-25 16:57:04 -0300

    #1979. Small fix.

--- test/commands.lua
+++ test/commands.lua
@@ -3,7 +3,7 @@ time = true -- show execution time for each command
 
 unnecessary  = {
 	doc        = {arg = "-doc -color"},
-	script     = {script = "scripts/basic.lua -strict"},
+	script     = {script = "basic.lua -strict"},
 	sketch     = {arg = "-sketch -color"},
 	version    = {arg = "-version -color"},
 	showdoc    = {arg = "-showdoc -color"},
