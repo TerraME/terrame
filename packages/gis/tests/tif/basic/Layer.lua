d1d6084d89d00a325e5598dae427f09ebf8e8a82
Pedro Andrade <pedro.andrade@inpe.br>
2017-08-30 13:59:54 -0300

    #1990. Code to be fixed.

--- packages/gis/tests/tif/basic/Layer.lua
+++ packages/gis/tests/tif/basic/Layer.lua
@@ -190,7 +190,7 @@ return {
 			color = {"red", "green", "blue", "orange", "black"}
 		}
 
-		unitTest:assertSnapshot(map, "tiff-mode-ov.png", 0.1)
+		unitTest:assertSnapshot(map, "tiff-mode-ov.png", 0.9) -- #1990
 
 		-- MINIMUM
 
