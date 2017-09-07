22bc57d48b2cec62abf4247a4271766adb233c68
Rodrigo Avancini <avancinirodrigo@yahoo.com.br>
2017-09-07 09:36:30 -0300

    terrame-unittest-windows-10.bat done #1924.

--- jenkins/win/terrame-unittest-windows-10.bat
+++ jenkins/win/terrame-unittest-windows-10.bat
@@ -19,7 +19,7 @@
 :: In no event shall INPE and TerraLAB / UFOP be held liable to any party for direct,
 :: indirect, special, incidental, or consequential damages arising out of the use
 :: of this software and its documentation.
-exit
+
 :: 
 :: It performs a TerraME functional test of any package. For TerraME purporses, "base" and "gis" internal packages. 
 :: It may be useful for TerraME external packages.
@@ -34,8 +34,6 @@ exit
 :: Exporting enviroment variables
 set "TME_PATH=%_TERRAME_INSTALL_PATH%\bin"
 set "PATH=%PATH%;%TME_PATH%"
-
-cd %_TERRAME_TEST_DIR%
 
 terrame -version
 
