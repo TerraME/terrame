ba5d3f190d256830f52ff33cfe39692d5d77da56
Pedro Andrade <pedro.andrade@inpe.br>
2017-10-03 15:07:31 -0300

    Small update related to #2032.

--- packages/base/lua/Model.lua
+++ packages/base/lua/Model.lua
@@ -647,13 +647,13 @@ function Model(attrTab)
 					elseif value.max and argv[name] > value.max then
 						customError("Argument "..toLabel(name).." should be less than or equal to "..value.max..".")
 					elseif value.step then
-						local remainder = (((argv[name] - value.min)) * 1000) % (value.step * 1000)
+						local remainder = (argv[name] - value.min) % value.step
 
-						if math.abs(remainder - value.step * 1000) < sessionInfo().round then
+						if math.abs(remainder - value.step) < sessionInfo().round then
 							-- There is a problem in Lua with operator % using numbers between 0 and 1
 							-- For example, 0.7 % 0.1 == 0.1, but should be 0.0. That's why we need
 							-- to multiplicate by 1000 above
-							remainder = remainder - value.step * 1000
+							remainder = remainder - value.step
 						end
 
 						if math.abs(remainder) > sessionInfo().round then
