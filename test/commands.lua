fc7183515bb4cc20626f85439dee8dec6580df18
Pedro Andrade <pedro.andrade@inpe.br>
2017-10-16 16:38:56 -0200

    Fixing small bug in execution test commands.

--- test/commands.lua
+++ test/commands.lua
@@ -43,7 +43,7 @@ test = {
 	linedirectory  = {arg = "-test", config = "linesDirectory.lua"},
 	testnotest     = {arg = "-test", config = "testNoTest.lua"},
 	notest         = {arg = "-test", config = "noTest.lua"},
-	unittest       = {arg = "-test", package = "unittest"}
+	unittest       = {arg = "-test", package = "unittest"},
 	singlefile     = {arg = "-test", config = "single.lua", package = "gis"}, -- file that does not belong to the source code
 }
 
