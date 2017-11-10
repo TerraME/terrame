ad7271d7df3679aec26e6399495cbeedc16cc692
Pedro Andrade <pedro.andrade@inpe.br>
2017-11-10 11:19:08 -0200

    Close #2058. Including examples from Modeling Paradigms.

---
+++ ide/zerobrane/terrame-examples/modeling-paradigms/00-contents.lua
@@ -0,0 +1,12 @@
+--[[ [previous](00-contents.lua) | [contents](00-contents.lua) | [next](Tub.lua)
+
+# Modeling Paradigms
+
+This page presents the models implemented in the tutorial _Modeling Paradigms_,
+available at https://github.com/TerraME/terrame/wiki/Paradigms.
+
+(1) [Tub](Tub.lua)
+(2) [Fire](Fire.lua)
+(3) [Growing Society](GrowingSociety.lua)
+
+]]
---
+++ ide/zerobrane/terrame-examples/modeling-paradigms/Fire.lua
@@ -0,0 +1,54 @@
+--[[ [previous](Tub.lua) | [contents](00-contents.lua) | [next](GrowingSociety.lua) ]]
+
+Fire = Model{
+    finalTime = 50,
+    dim = 50,
+    init = function(model)
+        model.cell = Cell{
+            state = "forest",
+            execute = function(self)
+                if self.past.state == "burning" then
+                    self.state = "burned"
+                elseif self.past.state == "forest" then
+                    forEachNeighbor(self, function(neigh)
+                        if neigh.past.state == "burning" then
+                            self.state = "burning"
+                        end
+                    end)
+                end
+            end
+        }
+
+        model.cs = CellularSpace{
+            xdim = model.dim,
+            instance = model.cell,
+        }
+
+        model.cs:createNeighborhood{strategy = "vonneumann"}
+        model.cs:sample().state = "burning"
+
+        model.chart = Chart{
+            target = model.cs,
+            select = "state",
+            value = {"forest", "burning", "burned"},
+            color = {"green", "red", "brown"}
+        }
+
+        model.map = Map{
+            target = model.cs,
+            select = "state",
+            value = {"forest", "burning", "burned"},
+            color = {"green", "red", "brown"}
+        }
+
+        model.timer = Timer{
+            Event{action = model.cs},
+            Event{action = model.chart},
+            Event{action = model.map}
+        }
+    end
+}
+
+fire = Fire{}
+fire:run()
+
