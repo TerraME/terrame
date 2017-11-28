b5323751fcda6f91c23799638100cbd0fff1ef6f
Rodrigo Avancini <avancinirodrigo@yahoo.com.br>
2017-11-28 16:55:27 -0200

    Close #2076 - Unnecessary details link in pull requests.

--- jenkins/linux/terrame-environment-linux-ubuntu-14.04.sh
+++ jenkins/linux/terrame-environment-linux-ubuntu-14.04.sh
@@ -64,25 +64,25 @@ echo ""
 ####################### GitHub Triggers
 if [ ! -z "$ghprbActualCommit" ]; then
 	echo "Triggering All Builds"
-	$_TERRAME_BUILD_BASE/solution/terrame-git-notify-linux-ubuntu-14.04.sh $ghprbActualCommit "C++ Syntax" -2 "$BUILD_URL/consoleFull"
+	$_TERRAME_BUILD_BASE/solution/terrame-git-notify-linux-ubuntu-14.04.sh $ghprbActualCommit "C++ Syntax" -2 ""
 	sleep 1s
-	$_TERRAME_BUILD_BASE/solution/terrame-git-notify-linux-ubuntu-14.04.sh $ghprbActualCommit "Linux Compilation" -2 "$BUILD_URL/consoleFull"
+	$_TERRAME_BUILD_BASE/solution/terrame-git-notify-linux-ubuntu-14.04.sh $ghprbActualCommit "Linux Compilation" -2 ""
 	sleep 1s
 	$_TERRAME_BUILD_BASE/solution/terrame-git-notify-linux-ubuntu-14.04.sh $ghprbActualCommit "C++ Unit Testing" -2 ""
 	sleep 1s
-	$_TERRAME_BUILD_BASE/solution/terrame-git-notify-linux-ubuntu-14.04.sh $ghprbActualCommit "Code analysis of package base" -2 "$BUILD_URL/consoleFull"
+	$_TERRAME_BUILD_BASE/solution/terrame-git-notify-linux-ubuntu-14.04.sh $ghprbActualCommit "Code analysis of package base" -2 ""
 	sleep 1s
-	$_TERRAME_BUILD_BASE/solution/terrame-git-notify-linux-ubuntu-14.04.sh $ghprbActualCommit "Code analysis of package gis" -2 "$BUILD_URL/consoleFull"
+	$_TERRAME_BUILD_BASE/solution/terrame-git-notify-linux-ubuntu-14.04.sh $ghprbActualCommit "Code analysis of package gis" -2 ""
 	sleep 1s
-	$_TERRAME_BUILD_BASE/solution/terrame-git-notify-linux-ubuntu-14.04.sh $ghprbActualCommit "Documentation of package base" -2 "$BUILD_URL/consoleFull"
+	$_TERRAME_BUILD_BASE/solution/terrame-git-notify-linux-ubuntu-14.04.sh $ghprbActualCommit "Documentation of package base" -2 ""
 	sleep 1s
-	$_TERRAME_BUILD_BASE/solution/terrame-git-notify-linux-ubuntu-14.04.sh $ghprbActualCommit "Documentation of package gis" -2 "$BUILD_URL/consoleFull"
+	$_TERRAME_BUILD_BASE/solution/terrame-git-notify-linux-ubuntu-14.04.sh $ghprbActualCommit "Documentation of package gis" -2 ""
 	sleep 1s
-	$_TERRAME_BUILD_BASE/solution/terrame-git-notify-linux-ubuntu-14.04.sh $ghprbActualCommit "Functional test of package base" -2 "$BUILD_URL/consoleFull"
+	$_TERRAME_BUILD_BASE/solution/terrame-git-notify-linux-ubuntu-14.04.sh $ghprbActualCommit "Functional test of package base" -2 ""
 	sleep 1s
-	$_TERRAME_BUILD_BASE/solution/terrame-git-notify-linux-ubuntu-14.04.sh $ghprbActualCommit "Functional test of package gis" -2 "$BUILD_URL/consoleFull"
+	$_TERRAME_BUILD_BASE/solution/terrame-git-notify-linux-ubuntu-14.04.sh $ghprbActualCommit "Functional test of package gis" -2 ""
 	sleep 1s
-	$_TERRAME_BUILD_BASE/solution/terrame-git-notify-linux-ubuntu-14.04.sh $ghprbActualCommit "Execution Test" -2 "$BUILD_URL/consoleFull"
+	$_TERRAME_BUILD_BASE/solution/terrame-git-notify-linux-ubuntu-14.04.sh $ghprbActualCommit "Execution Test" -2 ""
 fi
 
 cp --verbose $_TERRAME_GIT_DIR/jenkins/linux/terrame-repository-test-linux-ubuntu-14.04.sh $_TERRAME_REPOSITORY_DIR
