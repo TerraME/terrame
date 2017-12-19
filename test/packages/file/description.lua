fad8bd3118c25c04314a4afad04cf79ab5724b28
wsenafranca <wsenafranca@gmail.com>
2017-12-19 10:45:41 -0200

    Close #2069 - Verify created directories in the end of the tests

---
+++ test/packages/file/description.lua
@@ -0,0 +1,8 @@
+version = "0.1"
+license = "LGPL-3"
+date = "18 Dec 2017"
+package = "file"
+title = "File tests for TerraME"
+authors = "Pedro R. Andrade"
+contact = "pedro.andrade@inpe.br"
+content = [[Package to test whether every file and directory created during tests are really being removed.]]
