38284d0537e4894c9000e8d2d04fa9cc29c16db7
Pedro R. Andrade <pedro.andrade@inpe.br>
2018-04-17 16:40:10 -0300

    Version updated to rc7.

--- packages/base/tests/functional/basics/Package.lua
+++ packages/base/tests/functional/basics/Package.lua
@@ -63,7 +63,7 @@ return{
 	packageInfo = function(unitTest)
 		local r = packageInfo()
 
-		unitTest:assertEquals(r.version, "2.0.0-rc7-dev")
+		unitTest:assertEquals(r.version, "2.0.0-rc7")
 		unitTest:assertEquals(r.package, "base")
 		unitTest:assertEquals(r.url, "http://www.terrame.org")
 		unitTest:assertType(r.data, "Directory")
