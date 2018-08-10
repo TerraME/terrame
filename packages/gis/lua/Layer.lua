031cb8adb2b83ed8b6471c475d6786151051a48c
Pedro R. Andrade <pedro.andrade@inpe.br>
2018-08-10 15:10:09 -0300

    Close #2234. Fixing error when creating a Layer withou using argument project.

--- packages/gis/lua/Layer.lua
+++ packages/gis/lua/Layer.lua
@@ -1429,11 +1429,12 @@ metaTableLayer_ = {
 function Layer(data)
 	verifyNamedTable(data)
 	mandatoryTableArgument(data, "name", "string")
-	if type(data.project) == "string" then
-		data.project = File(data.project)
-	end
 
-	if type(data.project) == "File" then
+	local pType = type(data.project)
+
+	if pType == "string" then
+		data.project = File(data.project)
+	elseif pType == "File" then
 		if not data.project:exists() then
 			customError("Project file '"..data.project.."' does not exist.")
 		end
@@ -1441,6 +1442,8 @@ function Layer(data)
 		data.project = Project{
 			file = data.project
 		}
+	elseif pType ~= "Project" then
+		mandatoryTableArgument(data, "project", "Project")
 	end
 
 	if data.file and type(data.file) == "string" then
