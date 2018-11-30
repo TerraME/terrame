65c2a2fd5e51d2aef7621f24c7e561010f8cf9d9
Rodrigo Avancini <avancinirodrigo@yahoo.com.br>
2018-11-30 17:01:08 -0200

    Wfs test #1947.

---
+++ packages/gis/tests/wfs/basic/Project.lua
@@ -0,0 +1,69 @@
+-------------------------------------------------------------------------------------------
+-- TerraME - a software platform for multiple scale spatially-explicit dynamic modeling.
+-- Copyright (C) 2001-2017 INPE and TerraLAB/UFOP -- www.terrame.org
+
+-- This code is part of the TerraME framework.
+-- This framework is free software; you can redistribute it and/or
+-- modify it under the terms of the GNU Lesser General Public
+-- License as published by the Free Software Foundation; either
+-- version 2.1 of the License, or (at your option) any later version.
+
+-- You should have received a copy of the GNU Lesser General Public
+-- License along with this library.
+
+-- The authors reassure the license terms regarding the warranties.
+-- They specifically disclaim any warranties, including, but not limited to,
+-- the implied warranties of merchantability and fitness for a particular purpose.
+-- The framework provided hereunder is on an "as is" basis, and the authors have no
+-- obligation to provide maintenance, support, updates, enhancements, or modifications.
+-- In no event shall INPE and TerraLAB / UFOP be held liable to any party for direct,
+-- indirect, special, incidental, or consequential damages arising out of the use
+-- of this software and its documentation.
+--
+-------------------------------------------------------------------------------------------
+
+return {
+	Project = function(unitTest)
+		local insertNewLayerQgis = function()
+			local qgpfile = filePath("test/sampa_v3.qgs", "gis")
+			local spfile = filePath("test/sampa.shp", "gis")
+			qgpfile:copy(currentDir())
+			spfile:copy(currentDir())
+
+			local qgp = Project {
+				file = "sampa_v3.qgs",
+				user = "postgres",
+				password = "postgres"
+			}
+
+			local l2 = Layer{
+				project = qgp,
+				name = "LayerWFS",
+				service = "http://terrabrasilis.info/redd-pac/wfs",
+				feature = "reddpac:wfs_biomes"
+			}
+
+			local qgp2 =  Project{
+				file = "sampa_v3.qgs"
+			}
+
+			local l3 = Layer{
+				project = qgp2,
+				name = l2.name
+			}
+
+			unitTest:assertEquals(l3.name, l2.name)
+			unitTest:assertEquals(l3.source, "wfs")
+			unitTest:assertEquals(l3.service, l2.service)
+			unitTest:assertEquals(l3.feature, l2.feature)
+			unitTest:assertEquals(l3.epsg, l2.epsg)
+			unitTest:assertEquals(l3.encoding, l2.encoding)
+
+			qgp.file:delete()
+			File("sampa_v3.tview")
+			File("sampa.shp"):delete()
+		end
+
+		unitTest:assert(insertNewLayerQgis)
+	end
+}
