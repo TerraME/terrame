a1417901678f0c1efbf22fe4d76fecf2a38e153b
Rodrigo Avancini <avancinirodrigo@yahoo.com.br>
2019-02-13 11:03:08 -0200

    Adding test and correction #2280.

--- packages/base/lua/File.lua
+++ packages/base/lua/File.lua
@@ -232,7 +232,7 @@ File_ = {
 			qix:deleteIfExists()
 		elseif self:extension() == "qgs" then
 			local tview = File(string.sub(self.filename, 1, -4).."tview")
-			tview:delete():deleteIfExists()
+			tview:deleteIfExists()
 		end
 	end,
 	--- Remove a file if it exists. It does not stop with an error when the file does not exist.
