363f7134561601c67b030685e9be57a06b88bf8f
Rodrigo Avancini <avancinirodrigo@yahoo.com.br>
2019-12-02 10:58:05 -0300

    small change.

--- jenkins/linux/terrame-environment-ubuntu.sh
+++ jenkins/linux/terrame-environment-ubuntu.sh
@@ -56,6 +56,8 @@ cp --verbose $_TERRAME_GIT_DIR/jenkins/linux/terrame-git-notify-ubuntu.sh $_TERR
 valid $? "Error: Copying fail"
 cp --verbose $_TERRAME_GIT_DIR/jenkins/linux/terrame-installer-ubuntu.sh $_TERRAME_BUILD_BASE/solution
 valid $? "Error: Copying fail"
+cp --verbose $_TERRAME_GIT_DIR/jenkins/linux/terrame-terralib-ubuntu.sh $_TERRAME_BUILD_BASE/solution
+valid $? "Error: Copying fail"
 
 echo ""
 echo ""
