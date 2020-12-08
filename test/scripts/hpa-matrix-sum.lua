074cc29740d981bc2d451d09a3834e2cb0ec8aa9
Rodrigo Avancini <avancinirodrigo@yahoo.com.br>
2020-12-08 17:24:05 -0300

    handling memory on hpa.

--- test/scripts/hpa-matrix-sum.lua
+++ test/scripts/hpa-matrix-sum.lua
@@ -1,112 +1,115 @@
-local hpa = HPA()
+do
+	local hpa = HPA()
 
-local function setupToSum(np, dim)
-	local t0 = os.clock()
-	
-	local csA = CellularSpace {
-		xdim = dim
-	}
+	local function setupToSum(np, dim)
+		local t0 = os.clock()
+		
+		local csA = CellularSpace {
+			xdim = dim
+		}
 
-	local csB = CellularSpace {
-		xdim = dim
-	}
+		local csB = CellularSpace {
+			xdim = dim
+		}
 
-	local csC = CellularSpace {
-		xdim = dim
-	}
+		local csC = CellularSpace {
+			xdim = dim
+		}
 
-	local csD = CellularSpace {
-		xdim = dim
-	}
+		local csD = CellularSpace {
+			xdim = dim
+		}
 
-	function fillCsA()
-		forEachCell(csA, function(cell)
-			cell.value = dim
-		end)
+		function fillCsA()
+			forEachCell(csA, function(cell)
+				cell.value = dim
+			end)
+		end
+
+		function fillCsB()
+			forEachCell(csB, function(cell)
+				cell.value = dim
+			end)
+		end
+
+		function fillCsC()
+			forEachCell(csC, function(cell)
+				cell.value = dim
+			end)
+		end
+
+		function fillCsD()
+			forEachCell(csD, function(cell)
+				cell.value = dim
+			end)
+		end
+
+		hpa:np(np)
+
+		local t1 = os.clock()
+
+		hpa:parallel("fillCsA()")
+		hpa:parallel("fillCsB()")
+		hpa:parallel("fillCsC()")
+		hpa:parallel("fillCsD()")
+		hpa:joinall()
+
+		io.flush()
+		local t2 = os.clock()
+
+		print(string.format("setup elapsed time (parallel): %.3f", t2 - t1))
+		print(string.format("setup elapsed time (total): %.3f", t2 - t0))
+
+		return csA, csB, csC, csD
 	end
 
-	function fillCsB()
-		forEachCell(csB, function(cell)
-			cell.value = dim
-		end)
+	local function matrixSum(np, dim, csA, csB, csC, csD)
+		local t0 = os.clock()
+
+		local res = CellularSpace {
+			xdim = dim
+		}
+
+		function sum(i)
+			for j = 0, dim - 1 do
+				res:get(i, j).value = csA:get(i, j).value + csB:get(i, j).value
+									+ csC:get(i, j).value + csD:get(i, j).value
+			end
+		end
+
+		hpa:np(np)
+
+		local t1 = os.clock()
+
+		for i = 0, dim - 1 do
+			hpa:parallel("sum(i)", i)
+		end
+
+		hpa:joinall()
+
+		io.flush()
+		local t2 = os.clock()
+
+		print(string.format("sum elapsed time (parallel): %.3f", t2 - t1))
+		print(string.format("sum elapsed time (total): %.3f", t2 - t0))
+
+		return res:sample().value
 	end
 
-	function fillCsC()
-		forEachCell(csC, function(cell)
-			cell.value = dim
-		end)
-	end
+	local np = hpa:np() - 1 -- time increase using all processors
+	if np > 3 then np = 3 end
+	local dim = 500
 
-	function fillCsD()
-		forEachCell(csD, function(cell)
-			cell.value = dim
-		end)
-	end
-
-	hpa:np(np)
-
-	local t1 = os.clock()
-
-	hpa:parallel("fillCsA()")
-	hpa:parallel("fillCsB()")
-	hpa:parallel("fillCsC()")
-	hpa:parallel("fillCsD()")
-	hpa:joinall()
-
-	io.flush()
-	local t2 = os.clock()
-
-	print(string.format("setup elapsed time (parallel): %.3f", t2 - t1))
-	print(string.format("setup elapsed time (total): %.3f", t2 - t0))
-
-	return csA, csB, csC, csD
-end
-
-local function matrixSum(np, dim, csA, csB, csC, csD)
-	local t0 = os.clock()
-
-	local res = CellularSpace {
-		xdim = dim
-	}
-
-	function sum(i)
-		for j = 0, dim - 1 do
-			res:get(i, j).value = csA:get(i, j).value + csB:get(i, j).value
-								+ csC:get(i, j).value + csD:get(i, j).value
+	for n = 1, np do
+		print("number of processors: "..n)
+		local csA, csB, csC, csD = setupToSum(n, dim)
+		collectgarbage("collect")
+		local sample = matrixSum(n, dim, csA, csB, csC, csD)
+		collectgarbage("collect")
+		if sample ~= 4*dim then
+			print("Error", sample, 4*dim)
+			os.exit(1)
 		end
 	end
-
-	hpa:np(np)
-
-	local t1 = os.clock()
-
-	for i = 0, dim - 1 do
-		hpa:parallel("sum(i)", i)
-	end
-
-	hpa:joinall()
-
-	io.flush()
-	local t2 = os.clock()
-
-	print(string.format("sum elapsed time (parallel): %.3f", t2 - t1))
-	print(string.format("sum elapsed time (total): %.3f", t2 - t0))
-
-	return res:sample().value
 end
-
-local np = hpa:np() - 1 -- time increase using all processors
-if np > 3 then np = 3 end
-local dim = 500
-
-for n = 1, np do
-	print("number of processors: "..n)
-	local csA, csB, csC, csD = setupToSum(n, dim)
-	collectgarbage("collect")
-	local sample = matrixSum(n, dim, csA, csB, csC, csD)
-	collectgarbage("collect")
-	if sample ~= 4*dim then
-		print("Error", sample, 4*dim)
-		os.exit(1)
-	end
-end
+collectgarbage("collect")
